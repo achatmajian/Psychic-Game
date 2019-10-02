14542f2c2fcc2b987b2e507a45f54ff89f2eba7d
Arek Chatmajian <arek.chatmajian@gmail.com>
2019-10-02 18:59:56 -0400

    Put some finishing touches to get it working with Ben before class on 10/2/2019

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,57 @@
 // Establishing variable of alphabet array
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Establishing variables for wins, losses, guesses left which all begin at 0
 //Establishes the random letter the computer selects, while showing it in the browser's console log
 var wins = 0;
 var losses = 0;
-var guesses = 0;
-var guessesLog = "";
+var guesses = 9;
+var guessesLog = [];
 
 //Sometimes the computer picks the same letter twice in a row, is there a way to avoid that?
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)]
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-console.log(computerChoice)
+console.log(computerChoice);
 
 //Function for user keypress
-document.onKeyDown() = function(event) {
+document.onkeydown = function(event) {
     var userGuess = event.key;
+    guessesLog.push(userGuess);
 
     //if user guess is the same as computer choice, +1 to "wins"
     if (userGuess === computerChoice) {
         wins++;
+        // alert("You win!");
+        // computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        // guesses = 9;
+        // guessesLog = "";
+        reset("win");
     }
     // if user guess is not the same as computer choice, -1 to "guesses"
-    else (userGuess !== computerChoice) {
+    else {
         guesses--;
     }
     //if user reaches 0 guesses left, +1 to "losses"
     if (guesses === 0) {
         losses++;
+        // alert("You lose!");
+        // computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        // guesses = 9;
+        // guessesLog = "";
+        reset("lose");
     }
 
     // Get element by ID in order to update numbers for wins, losses, and guesses
     document.getElementById("wins").innerHTML = "Wins: " + wins;
     document.getElementById("losses").innerHTML = "losses: " + losses;
     document.getElementById("guesses").innerHTML = "Guesses left: " + guesses;
-    
-    // Keylog function for "Your guesses so far"
-    document.addEventListener("keypress", logKey);
-    function logKey() {
-    log.guessesLog += ${e.code};
-    document.getElementById("guessesLog").innerHTML = "Your guesses so far: " + guessesLog; 
-}
-
+    document.getElementById("guessesLog").innerHTML = "Your guesses so far: " + guessesLog.join(', '); 
+ 
+    function reset(result) {
+        alert("You " + result + "!");
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        guesses = 9;
+        guessesLog = [];
+    }
 }
